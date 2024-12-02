a7871c2005b160c0823c43c15c814cc4c605ef91
IntegralHamster <75609426+IntegralHamster@users.noreply.github.com>
2024-12-02 12:45:56 +0200

    Update 2024-2.py

--- 2024-2.py
+++ 2024-2.py
@@ -2,33 +2,29 @@ with open('input.txt') as f:
     lines = [line.strip() for line in f.readlines()]
 def line_check(report):
     flag = 0
-    unsafefl = 0
     for i in range(len(report) - 1):
         if 1 <= int(report[i+1]) - int(report[i]) <= 3:
             if flag == 0:
                 flag = 1
             if flag < 0:
-                unsafefl = 1
-                break
+                return 0
             if flag > 0:
                 continue
         elif -3 <= int(report[i+1]) - int(report[i]) <= -1:
             if flag == 0:
                 flag = -1
             if flag > 0:
-                unsafefl = 1
-                break
+                return 0
             if flag < 0:
                 continue
         else:
-            unsafefl = 1
-            break
-    return unsafefl
+            return 0
+    return 1
 
 safe = []
 for line in lines:
     report = line.split()
-    safe.append(1 - line_check(report))
+    safe.append(line_check(report))
 
 safe2 = []
 for j in range(len(lines)):
@@ -36,15 +32,15 @@ for j in range(len(lines)):
         safe2.append(1)
         continue
     else:
-        found = 0
+        found = 1
         report = lines[j].split()
         for k in range(len(report)):
             check_list = report.copy()
             del check_list[k]
             found = line_check(check_list)
-            if found == 0:
+            if found == 1:
                 break
-        if found == 0:
+        if found == 1:
             safe2.append(1)
             continue
         else:
