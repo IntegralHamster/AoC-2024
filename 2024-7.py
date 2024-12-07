13be907f6565df2e02ff6e2aad42f087c02ce27a
IntegralHamster <75609426+IntegralHamster@users.noreply.github.com>
2024-12-07 09:29:36 +0200

    Add files via upload

---
+++ 2024-7.py
@@ -0,0 +1,46 @@
+with open('input.txt') as f:
+    lines = [line.strip() for line in f.readlines()]
+
+goal = []
+numbers = []
+for line in lines:
+    left, right = line.split(': ')
+    goal.append(int(left))
+    numbers.append(right.split(' '))
+
+import re
+
+def line_combinations (num_inp):
+    if len(num_inp) == 2:
+        return [num_inp[0]+'+'+num_inp[1], num_inp[0]+'*'+num_inp[1], num_inp[0]+'~'+num_inp[1]]
+    else:
+        ans = []
+        for option in line_combinations(num_inp[1:]):
+            ans.append(num_inp[0] + '+' + option)
+            ans.append(num_inp[0] + '*' + option)
+            ans.append(num_inp[0] + '~' + option)
+        return ans
+
+def evaluate_line (expression):
+    ans = 0
+    for i in range(len(expression)):
+        if i == 0:
+            ans += int(expression[i])
+        if expression[i] == '+':
+            ans += int(expression[i+1])
+        if expression[i] == '*':
+            ans *= int(expression[i+1])
+        if expression[i] == '~':
+            ans = int(str(ans) + expression[i + 1])
+    return ans
+
+part12 = 0
+for i in range(len(goal)):
+    for option in line_combinations(numbers[i]):
+        if goal[i] == evaluate_line(re.split(r'(\W)', option)):
+            part12 += goal[i]
+            break
+
+print(part12)
+
+# for part 1 comment out the ~ parts in line_combinations. Expect runtime of around 30-40 seconds for part 2, faster for part 1
